0d6f974e924cdd11d9f46508d9649054f76df37f
Albert Viilik <albert.viilik@student.manchester.ac.uk>
2019-03-17 13:02:54 +0000

    Sample code run

--- youtube.js
+++ youtube.js
@@ -18,3 +18,29 @@ youtube.searchVideos('sandu ciorba', 4)
             .pipe(fs.createWriteStream(results[1].id + '.mp3'));
 
     }).catch(console.log);
+
+async function quickstart(
+  projectId = 'hacktheburgh2019', // Your Google Cloud Platform project ID
+  bucketName = 'hacktheburghfiles' // The name for the new bucket
+) {
+  // Imports the Google Cloud client library
+  const {Storage} = require('@google-cloud/storage');
+
+  // Creates a client
+  const storage = new Storage({projectId});
+
+  // Creates the new bucket
+  await storage.createBucket(bucketName);
+  console.log(`Bucket ${bucketName} created.`);
+  await storage.bucket(bucketName).upload(filename, {
+  // Support for HTTP requests made with `Accept-Encoding: gzip`
+  gzip: true,
+  metadata: {
+    // Enable long-lived HTTP caching headers
+    // Use only if the contents of the file will never change
+    // (If the contents will change, use cacheControl: 'no-cache')
+    cacheControl: 'public, max-age=31536000',
+  },
+});
+  console.log(`${filename} uploaded to ${bucketName}.`);
+}
