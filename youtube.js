bef475070b675575f12fc1dda5044fc68c7dd42c
Albert Viilik <albert.viilik@student.manchester.ac.uk>
2019-03-17 11:24:10 +0000

    Uploading to google storage and making public

--- youtube.js
+++ youtube.js
@@ -5,6 +5,11 @@ const apiKey = require('./youtube.key.js');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(apiKey.key);
 
+const {Storage} = require('@google-cloud/storage');
+
+const storage = new Storage();
+
+
 //youtube downloader
 const ytdl = require('ytdl-core');
 
@@ -18,3 +23,12 @@ youtube.searchVideos('sandu ciorba', 4)
             .pipe(fs.createWriteStream(results[1].id + '.mp3'));
 
     }).catch(console.log);
+
+await storage.bucket(hacktheburghfiles).upload(results[1].id + '.mp3', {
+  gzip: false,
+  metadata: {
+    cacheControl: 'public, max-age=31536000',
+  },
+});
+
+console.log(`${filename} uploaded to ${bucketName}.`);
