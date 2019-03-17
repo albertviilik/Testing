d9c474da7ec0bb727eb53a88d239185cefbd408e
Albert Viilik <albert.viilik@student.manchester.ac.uk>
2019-03-17 12:49:35 +0000

    changed

--- youtube.js
+++ youtube.js
@@ -5,12 +5,6 @@ const apiKey = require('./youtube.key.js');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(apiKey.key);
 
-const {Storage} = require('@google-cloud/storage');
-
-const storage = new Storage();
-const results = null;
-
-
 //youtube downloader
 const ytdl = require('ytdl-core');
 
@@ -24,25 +18,3 @@ youtube.searchVideos('sandu ciorba', 4)
             .pipe(fs.createWriteStream(results[1].id + '.mp3'));
 
     }).catch(console.log);
-
-async function quickstart(
-  projectId = 'hacktheburgh2019', // Your Google Cloud Platform project ID
-  bucketName = 'hacktheburghfiles' // The name for the new bucket
-) {
-  // Imports the Google Cloud client library
-  const {Storage} = require('@google-cloud/storage');
-
-  // Creates a client
-  const storage = new Storage({projectId});
-  console.log(`Bucket ${bucketName} exists.`);
-
-  await storage.bucket('hacktheburghfiles').upload(results[1].id + '.mp3', {
-  gzip: false,
-  metadata: {
-    cacheControl: 'public, max-age=31536000',
-  },
-});
-  console.log(`${filename} uploaded to ${bucketName}.`);
-}
-
-quickstart().catch();
